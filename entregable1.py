6bd183192557360ea2ba2b053df729a4ee1b1c81
MarcosJovani <marcos.jovani98@gmail.com>
2021-10-11 17:58:04 +0200

    no se si va, pero hay cositas

--- entregable1.py
+++ entregable1.py
@@ -2,6 +2,7 @@ import sys
 from typing import *
 
 from algoritmia.datastructures.digraphs import UndirectedGraph
+from algoritmia.datastructures.queues import Fifo
 
 Vertex = Tuple[int, int]
 Edge = Tuple[Vertex, Vertex]
@@ -29,6 +30,61 @@ def read_data(f) -> Tuple[Pair, UndirectedGraph]:
     return (rows, cols), UndirectedGraph(E=e)
 
 
+def crea_matriz(rows:int, cols: int) -> List[List[int]]:
+    matriz = []
+    for i in range(rows):
+        a = [0]*cols
+        matriz.append(a)
+    return matriz
+
+def matriz_distancia_desde_origen(size: Pair, grafo: UndirectedGraph) -> List[List[int]]:
+    queue = Fifo()
+    seen = set()
+    queue.push(((0, 0), (0, 0)))
+    seen.add(0, 0)
+    matriz = crea_matriz(size[0],size[1])
+    cont = 0
+    matriz[0][0] = cont
+    while len(queue)>0:
+        u, v = queue.pop()
+        cont += 1
+        for suc in grafo.succs(v):
+            if suc not in seen:
+                seen.add(suc)
+                matriz[suc[0]][suc[1]] = cont + d((suc[0],suc[1]),(size[0]-1, size[1]-1))
+                queue.push((v, suc))
+    return matriz
+
+def recorredor_aristas_anchura(grafo: UndirectedGraph, v_inicial: Vertex) -> List[Edge]:
+    aristas = []
+    queue = Fifo()
+    seen = set()
+    queue.push((v_inicial, v_inicial))
+    seen.add(v_inicial)
+    while len(queue)>0:
+        u, v = queue.pop()
+        aristas.append((u, v))
+        for suc in grafo.succs(v):
+            if suc not in seen:
+                seen.add(suc)
+                queue.push((v, suc))
+    return aristas
+
+def recuperador_camino(lista_aristas: List[Edge], v: Vertex) -> List[Vertex]:
+    bp = {}
+    for orig,dest in lista_aristas:
+        bp[dest] = orig
+    camino = []
+    camino.append(v)
+    while v != bp[v]:
+        v = bp[v]
+        camino.append(v)
+    camino.reverse()
+    return camino
+
+def contar_vertices(lista:List[int]) -> int:
+    return len(lista)-1
+
 # - Tiene dos parámetros,
 #   - El primero: una tupla con el tamaño del laberinto (num_rows, num_cols)
 #   - El segundo: el grafo del laberinto.
@@ -37,7 +93,22 @@ def read_data(f) -> Tuple[Pair, UndirectedGraph]:
 #    - Una lista de vértices con el camino desde la entrada hasta el tesoro.
 #    - Una lista de vértices con el camino desde el tesoro hasta la salida.
 def process(size: Pair, lab: UndirectedGraph) -> Tuple[Pair, Path, Path]:
-    pass
+    distancia_origen = matriz_distancia_desde_origen(size, lab)
+    max = distancia_origen[0][0]
+    maxr = 0
+    maxc = 0
+    for r in range(size[0]):
+        for c in range(size[1]):
+            aristasrc = recorredor_aristas_anchura(lab, (r, c))
+            distancia_destino = recuperador_camino(aristasrc, (size[0]-1, size[1]-1))
+            distancia = distancia_origen[r][c] + len(distancia_destino) - 1
+            if distancia > max:
+                max = distancia
+                maxr = r
+                maxc = c
+                camino_origen = recuperador_camino(recorredor_aristas_anchura(lab, (0, 0)), (r, c))
+                camino_destino = distancia_destino
+    return (maxr, maxc), camino_origen, camino_destino
 
 
 # - Tiene tres parámetros:
@@ -50,7 +121,10 @@ def process(size: Pair, lab: UndirectedGraph) -> Tuple[Pair, Path, Path]:
 #    - Longitud del camino desde la entrada hasta el tesoro (nº de vertices menos 1)
 #    - Longitud del camino desde el tesoro hasta la salida (nº de vertices menos 1)
 def show_results(pos: Pair, c1: Path, c2: Path):
-    pass
+    print(pos[0])
+    print(pos[1])
+    print(len(c1))
+    print(len(c2))
 
 
 # -----------------------------------------------------
